fe2744ffdf15acb78a74f4aca9935e0821b6265c
Syed Sulthan <syed_search@yahoo.com>
2016-05-05 19:23:53 +0000

    Add Hello

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # sample_app
 my Ruby Leaning APP. Will be Buggy. Do not use it.
 
+git commit -m
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,5 +10,19 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "Check the Title" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  
+  
 
 end
